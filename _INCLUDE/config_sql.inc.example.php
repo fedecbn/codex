39104f2922ab2a58f813515ce8c55dff16b2d1a1
Thomas Milon <thomas.milon@fcbn.fr>
2015-06-02 16:41:04 +0200

    file add : db config example

---
+++ _INCLUDE/config_sql.inc.example.php
@@ -0,0 +1,31 @@
+<?php
+//------------------------------------------------------------------------------//
+//    /_INCLUDE/CONFIG_SQL.INC.PHP                                              //
+//                                                                              //
+//  Application WEB 'EVAL'                                                      //
+//  Outil d’aide à l’évaluation de la flore                                     //
+//                                                                              //
+//  Version 1.00  10/08/14 - DariaNet                                           //
+//  Version 1.01  13/08/14 - MaJ schémas                                        //
+//  Version 1.02  18/08/14 - Aj sql_format_num                                  //
+//  Renseigner les paramètre de connexion et remplacer le nom du fichier par confi_sql.inc.php//
+//------------------------------------------------------------------------------//
+
+    define ("SQL_server", "");
+    define ("SQL_port", "");
+    define ("SQL_user", "");
+    define ("SQL_pass", "");
+    define ("SQL_base", "");
+
+    define ("SQL_schema_app", "applications");
+    define ("SQL_schema_lr", "liste_rouge");
+    define ("SQL_schema_eee", "eee");
+    define ("SQL_schema_ref", "referentiels");
+    define ("SQL_schema_lsi", "lsi");
+
+//------------------------------------------------------------------------------ FONCTIONS
+
+
+
+
+?>
