7d66b590a02c1fca474346d66877835eab0d6212
Thomas Milon <thomas.milon@fcbn.fr>
2015-09-18 18:49:16 +0200

    debug rubrique bug 4

--- flore/bugs/bug-submit.php
+++ flore/bugs/bug-submit.php
@@ -24,12 +24,13 @@ if (!$db) fatal_error ("Impossible de se connecter au serveur PostgreSQL.",false
 if (!empty ($id))                                                               // EDIT
 {
     $query="UPDATE ".SQL_schema_app.".bug SET ";
-    foreach ($_POST as $field => $val) 
+    foreach ($_POST as $field => $val) {
         if ($field!="id" ) $query.=$field."=".sql_format ($val).",";
         if ($field!="descr" ) $query.=$field."=".sql_format_quote ($val,'do').",";
         if ($field!="statut_descr" ) $query.=$field."=".sql_format_quote ($val,'do').",";
         if ($field!="id" ) $query.=$field."=".sql_format ($val).",";
-    $query=rtrim ($query,",");
+		}
+	$query=rtrim ($query,",");
     $query.=" WHERE id_bug=".$id;
     $result=pg_query ($db,$query) or die ("Erreur pgSQL : ".pg_result_error ($result));
 }
