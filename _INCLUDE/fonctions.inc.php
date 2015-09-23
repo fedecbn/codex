536b3362c715363b561336166126ca8fcd0b6545
Thomas Milon <thomas.milon@fcbn.fr>
2015-09-23 10:28:49 +0200

    gestion du renouvellement de la note

--- _INCLUDE/fonctions.inc.php
+++ _INCLUDE/fonctions.inc.php
@@ -1203,6 +1203,9 @@ function msg_pw($variable)
 	foreach ($tab_ref as $key => $value)
 		if ($variable[$value] === 't') $variable[$value] = "<td> Vous êtes référent sur cette rubrique </td>"; else $variable[$value] = "";
 	
+	// $note = "150921_Note_Codex.pdf";
+	$note = "150915_Note_ouverture_Codex.pdf";
+	
 	$message = "<html><head></head><body>
 			---CECI EST UN MAIL AUTOMATIQUE---
 		<br>----MERCI DE NE PAS Y REPONDRE----
@@ -1212,7 +1215,7 @@ function msg_pw($variable)
 			<br> Retrouvez également la liste des outils de la FCBN à l'adresse suivante : <a href=\"services.fcbn.fr\">services.fcbn.fr</a>
 			<br> Si vous notez une erreur dans vos informations personnelles, merci d'envoyer un mail à <a href=\"mailto:informatique@fcbn.fr\">informatique@fcbn.fr</a> à ce sujet.
 			
-			<br><br>Retrouvez la note d’information qui présente l'outil au lien suivant : <a href = \"".str_replace("www.","",$_SERVER["SERVER_NAME"])."/flore/home/210915_Note_Codex.pdf\">".str_replace("www.","",$_SERVER["SERVER_NAME"])."/flore/home/150915_Note_ouverture_Codex.pdf</a>
+			<br><br>Retrouvez la note d’information qui présente l'outil au lien suivant : <a href = \"".str_replace("www.","",$_SERVER["SERVER_NAME"])."/flore/home/".$note."\">".str_replace("www.","",$_SERVER["SERVER_NAME"])."/flore/home/150915_Note_ouverture_Codex.pdf</a>
 			
 			<br><br><b>Identifiants de connexion</b>
 			<table cellpadding=\"5\" border =\"solid 1px black\">
