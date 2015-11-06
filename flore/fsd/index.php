84d6e7098de735ec3729075da630a695f9b8fee8
thomas <thomas.milon@fcbn.fr>
2015-11-06 15:29:59 +0100

    rub fsd : add image + ohter

--- flore/fsd/index.php
+++ flore/fsd/index.php
@@ -37,7 +37,7 @@ ref_colonne_et_valeur ($id_page);
 if ($niveau <= 64) $desc = " bloque"; else $disa = $desc;
 if ($niveau <= 64) $disa = "disabled"; else $disa = null;
 
-$format = array("string"=>"string","float"=>"float","integer"=>"integer","date"=>"date");
+$format = array("string"=>"string","float"=>"float","integer"=>"integer","booleen"=>"booleen","date"=>"date");
 $oblig = array(""=>"","Oui"=>"Oui","Oui si"=>"Oui si","Non"=>"Non","NSP"=>"NSP");
 
 // /*------------------------------------------------------------------------------ MAIN*/
@@ -286,7 +286,7 @@ if ($niveau <= 64) $disa = "disabled"; else $disa = null;
 				LEFT JOIN fsd.lien_champs t3 ON t1.uid = t3.uid
 				INNER JOIN hierarchie t2 ON t2.id_from = t1.uid
 				)
-			SELECT uid, version, modl, ssmodl, cd, lib, descr, format, taille, \"oData\", \"vocaCtrl\", \"regleRens\", ex1, ex2, discussion, \"oTaxa\", \"oSynData\", \"oSynTaxa\", obj FROM hierarchie GROUP BY uid, version, modl, ssmodl, cd, lib, descr, format, taille, \"oData\", \"vocaCtrl\", \"regleRens\", ex1, ex2, discussion, \"oTaxa\", \"oSynData\", \"oSynTaxa\", obj;";
+			SELECT uid, version, modl, ssmodl, cd, lib, descr, format, taille, \"oData\", \"vocaCtrl\", \"regleRens\", ex1, ex2, discussion, \"oTaxa\", \"oSynData\", \"oSynTaxa\", obj FROM hierarchie GROUP BY uid, version, modl, ssmodl, cd, lib, descr, format, taille, \"oData\", \"vocaCtrl\", \"regleRens\", ex1, ex2, discussion, \"oTaxa\", \"oSynData\", \"oSynTaxa\", obj ORDER BY version DESC;";
             $result=pg_query ($db,$query) or fatal_error ("Erreur pgSQL : ".pg_result_error ($result),false);
 
 			if (pg_num_rows ($result)) {
