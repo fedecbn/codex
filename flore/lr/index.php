21152ff3f61be8c59e943989d026cad96a175698
Thomas Milon <thomas.milon@fcbn.fr>
2016-02-11 14:28:09 +0100

    Rub LR : réorganisation JS et rafraîchissement des caches toutes les heures

--- flore/lr/index.php
+++ flore/lr/index.php
@@ -49,10 +49,7 @@ ref_colonne_et_valeur ($id_page);
 <script type="text/javascript" language="javascript" src="../../_INCLUDE/js/jquery-te-1.4.0.min.js"></script>
 <script type="text/javascript" language="javascript" src="../../_INCLUDE/js/icheck.min.js"></script>
 
-<script type="text/javascript" language="javascript" src="../../_INCLUDE/js/gestion.js"></script>
-<script type="text/javascript" language="javascript" src="js/liste.js"></script>
-<script type="text/javascript" language="javascript" src="js/autocomp.js"></script>
-
+<script type="text/javascript" language="javascript" src="js/commun.js"></script>
 <?php
 //------------------------------------------------------------------------------ MAIN
 // html_header ("utf-8","table_eval.css","jquery-te-1.4.0.css");
