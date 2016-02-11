a94853eb723ff14ff6f221e78323ca346b7c9311
Thomas Milon <thomas.milon@fcbn.fr>
2016-02-11 14:25:00 +0100

    Rub HUB : réorganisation JS et rafraîchissement des caches toutes les heures

--- flore/hub/index.php
+++ flore/hub/index.php
@@ -45,9 +45,7 @@ ref_colonne_et_valeur ($id_page);
 <script type="text/javascript" language="javascript" src="../../_INCLUDE/js/jquery-te-1.4.0.min.js"></script>
 <script type="text/javascript" language="javascript" src="../../_INCLUDE/js/icheck.min.js"></script>
 
-<script type="text/javascript" language="javascript" src="../../_INCLUDE/js/gestion.js"></script>
-<script type="text/javascript" language="javascript" src="js/liste.js"></script>
-<script type="text/javascript" language="javascript" src="js/autocomp.js"></script>
+<script type="text/javascript" language="javascript" src="js/commun.js"></script>
 
 <?php
 // /*------------------------------------------------------------------------------ MAIN*/
