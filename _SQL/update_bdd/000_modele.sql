9f06f62f52a7dff97a1c298b6dba243a6f7e8779
Thomas Milon <thomas.milon@fcbn.fr>
2016-03-18 15:43:32 +0100

    update_bdd : système de suivi des modification de la base de données
    Se référer à la documentation pour l'utilisation de ce système
    (docs/pour_les_administrateurs/Mise à jour de la base de données

---
+++ _SQL/update_bdd/000_modele.sql
@@ -0,0 +1,11 @@
+﻿CREATE OR REPLACE FUNCTION update_bdd() RETURNS varchar AS $BODY$ BEGIN
+
+--- 1. Code permettant la mise à jour
+-- ex : update...
+-- ex : insert...
+
+--- 2. Pour le suivi des mises à jours
+--- INSERT INTO applications.update_bdd VALUES (identifiant du fichier,numero du commit précédent, date);
+--- ex : INSERT INTO applications.update_bdd VALUES ('001','8285659184f39475917be396aaffb600287cc01c', '2016-03-18');
+
+RETURN 'OK';END;$BODY$ LANGUAGE plpgsql;SELECT * FROM update_bdd();
---
+++ _SQL/update_bdd/001.sql
@@ -0,0 +1,15 @@
+﻿CREATE OR REPLACE FUNCTION update_bdd() RETURNS varchar AS $BODY$ BEGIN
+
+--- 1. Code permettant la mise à jour
+CREATE TABLE applications.update_bdd (
+	id varchar,
+	commit varchar,
+	date date,
+	CONSTRAINT updt_pk PRIMARY KEY (id)
+);
+INSERT INTO applications.update_bdd VALUES ('0','initial commit', NOW());
+
+--- 2. Pour le suivi des mises à jours
+INSERT INTO applications.update_bdd VALUES ('001','8285659184f39475917be396aaffb600287cc01c', '2016-03-18');
+
+RETURN 'OK';END;$BODY$ LANGUAGE plpgsql;SELECT * FROM update_bdd();
