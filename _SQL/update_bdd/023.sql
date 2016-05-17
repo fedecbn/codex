f9494c44aaf368140d76e168c0b9cc534d0a5d0a
TomMilon <tom_milon@hotmail.com>
2016-05-17 10:42:30 +0200

    update_bdd : droit del hub

---
+++ _SQL/update_bdd/023.sql
@@ -0,0 +1,37 @@
+﻿CREATE OR REPLACE FUNCTION update_bdd() RETURNS varchar AS 
+$BODY$ 
+DECLARE phase varchar;
+DECLARE user_codex varchar;
+BEGIN
+----------------------------------------------------
+------VARIABLES A DÉFINIR---------------------------
+---## Pour tester la fonction. Une fois que vous souhaiter enregistrer la modif dans la table update_bdd, mettre la phase en "prod" ##--
+-- phase = 'test';
+phase = 'prod';
+---## user_codex est l'utilisateur du codex (décommentez la ligne suivante si besoin) ##--
+-- user_codex = 'pg_user';
+----------------------------------------------------
+
+
+--- 1. Code permettant la mise à jour
+UPDATE applications.droit SET role = 'evaluateur' WHERE typ_droit = 'd2' AND rubrique = 'hub' AND onglet = 'hub' AND objet = 'del';
+
+--- 2. Pour le suivi des mises à jours
+CASE phase 
+WHEN 'test' THEN RETURN 'Test OK';
+WHEN 'prod' THEN
+INSERT INTO applications.update_bdd (id, commit, descr, date) VALUES (
+	--- Numero du fichier (à modifier)
+	'023',
+	--- Numéro du dernier commit (à modifier)
+	'3aa69b572203500c1a3ac8a17bdd206f45854db9', 
+	--- Description de la modif BDD (à modifier)
+	'Ouverture des droits de suppression de la partie propre pour les évaluateurs',
+	--- Date (à ne pas modifier)
+	NOW());
+RETURN 'OK';
+ELSE RETURN 'Verifier la phase dans le code';
+END CASE;
+
+END;$BODY$ LANGUAGE plpgsql;
+SELECT * FROM update_bdd();
