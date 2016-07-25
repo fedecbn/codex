46ce836d0a2d83eb81d69d8e6821e6292397c0c8
anacaona83 <anacaona83@gmail.com>
2016-07-25 14:12:01 +0200

    home: commentaire sur le fichier install.php

--- flore/home/install.php
+++ flore/home/install.php
@@ -272,7 +272,7 @@ case "install-set":	{
 			{
 			if (isset($_POST[$key]))
 				{
-				if ($_POST[$key] == 'TRUE')
+				if ($_POST[$key] == 'TRUE') //si le bouton radio a été coché en "Oui"
 					{
 					$archi = "../../_SQL/bdd_codex_archi_$key.sql";
 					$data = "../../_SQL/bdd_codex_data_$key.sql";
@@ -293,7 +293,7 @@ case "install-set":	{
 
 			
 		/*------------------------------------------*/
-		/*parametrage du ficher de conf sql_connect*/
+		/*parametrage du ficher de conf config_sql.inc.php*/
 		if (!file_exists("../../_INCLUDE/config_sql.inc.php"))
 			{
 			$sql_file = file_get_contents("../../_INCLUDE/config_sql.inc.example.php");
